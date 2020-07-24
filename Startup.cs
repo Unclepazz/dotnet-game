4fe6855951f0b7d03a274f9195f4f1dea9e92379
unclepazz <unclepazz@gmail.com>
2020-07-24 19:10:59 +0100

    DataContext

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace dotnet_game
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext.DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
